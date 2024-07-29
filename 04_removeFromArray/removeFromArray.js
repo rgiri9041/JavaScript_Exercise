187eb7761450f27a38c25114bb220b671a41fc6f
Raj Kumar Giri <rgiri9041@gmail.com>
2024-07-29 16:21:37 +0545

    Exercise 04_removeElementFromArray is complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...rests) {
+    return array.filter(item => !rests.includes(item))    
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
