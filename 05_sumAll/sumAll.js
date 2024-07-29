ff1dd4a309d7b7693d239207fc70e3e4d2f87e74
Raj Kumar Giri <rgiri9041@gmail.com>
2024-07-29 17:26:12 +0545

    Exercise 05_sumAll complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function (num, num1) {
+    let sum = 0;
 
-};
+    if (typeof num !== 'number' || typeof num1 !== 'number') {
+        return "ERROR";
+    }
+    else if (num < 0 || num1 < 0) {
+        return "ERROR";
+    }
+    else if (num % 1 !== 0 || num1 % 1 !== 0) {
+        return "ERROR";
+    }
+    if (num > num1) {
+        [num, num1] = [num1,num]
+    }
+    for (let i = num; i <= num1; i++) {
+        sum += i;
+
+    }
+    return sum;
+};  
 
 // Do not edit below this line
 module.exports = sumAll;
