5c5d0bd1d3ebc5a15d1310904622623a3af7b4a3
Raj Kumar Giri <rgiri9041@gmail.com>
2024-07-30 13:20:30 +0545

    Exercise 09_palindormes complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,7 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let given = str.replace(/[^a-z0-9]/gi, '').toLowerCase();
+    let reverse = given.split('').reverse().join('');
+    return given === reverse
 
 };
 
