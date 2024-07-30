a0061c06331a9e6408fb6679ffd9dfc0461e7b7b
Raj Kumar Giri <rgiri9041@gmail.com>
2024-07-30 14:01:56 +0545

    Exercise 10_fionacci Complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let num = Number(n);
+    console.log(num)
+    if(num < 0){
+        return "OOPS";
+    }
+    if(num === 0){
+        return 0
+    }
+    if(num === 1){
+        return 1
+    }
+    let a =0, b=1
+    for(let i =1; i<num; i++){
+        let febb = a+b;
+        a =b
+        b= febb
+     }
+    return b;
 
 };
+let result = fibonacci("10");
+console.log(result);
 
 // Do not edit below this line
 module.exports = fibonacci;
