23c8384c4d2f64f02b92533fb240591b4809c901
Raj Kumar Giri <rgiri9041@gmail.com>
2024-07-30 12:24:33 +0545

    Exerccise 06_tempConversion Complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  let celsius = (fahrenheit - 32) / 1.8;
+  celsius = celsius.toFixed(1);
+  let conversion = parseFloat(celsius);
+  return conversion;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  let fahrenheit = (celsius * 1.8) + 32;
+  fahrenheit = fahrenheit.toFixed(1);
+  let fConversion = parseFloat(fahrenheit);
+  return fConversion;
+
 };
+//Celsius(°C)= (Fahrenheit(°F)−32) /1.8
+
 
 // Do not edit below this line
 module.exports = {
