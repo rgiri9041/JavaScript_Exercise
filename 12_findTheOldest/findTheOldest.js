cc16c6533f2f74f6121dcdee108fded898d28a6b
Raj Kumar Giri <rgiri9041@gmail.com>
2024-07-30 14:28:54 +0545

    Exercise 12_findTheOldest complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+      }
+      return death - birth;
+    };
+    
+    const getAge = function (people) {
+      return people.reduce((oldest, currentPerson) => {
+        const oldestAge = findTheOldest(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = findTheOldest(
+          currentPerson.yearOfBirth,
+          currentPerson.yearOfDeath
+        );
+        return oldestAge < currentAge ? currentPerson : oldest;
+      });
 };
 
 // Do not edit below this line
-module.exports = findTheOldest;
+module.exports = getAge;
